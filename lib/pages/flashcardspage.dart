78dc2a59d7ccf090df294b1ea679a3fc8e7e51d3
Tyler Ashcraft <d.tyler.ashcraft@gmail.com>
2025-02-23 18:19:26 -0500

    Fix bug with shuffle mixing word terms and definitions

--- lib/pages/flashcardspage.dart
+++ lib/pages/flashcardspage.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:gamebareru/pages/homepage.dart';
 
@@ -11,8 +9,7 @@ class FlashcardsPage extends StatefulWidget {
 }
 
 class _FlashcardsPageState extends State<FlashcardsPage> {
-  final List<String> _terms = List.empty(growable: true);
-  final List<String> _definitions = List.empty(growable: true);
+  final List<MapEntry<String, String>> wordSet = List.empty(growable: true);
   int _index = 0;
   bool _showDefinition = false;
 
@@ -22,23 +19,14 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
 
     for (int i = 0; i < HomePage.selectedWordSets.length; i++) {
       if (HomePage.selectedWordSets[i]) {
-        _terms.addAll(HomePage.wordSets[i].map.keys);
-        _definitions.addAll(HomePage.wordSets[i].map.values);
+        wordSet.addAll(HomePage.wordSets[i].map.entries);
       }
     }
   }
 
-  void shuffle() {
-    setState(() {
-      Random random = Random();
-      _terms.shuffle(random);
-      _definitions.shuffle(random);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (_terms.isEmpty) {
+    if (wordSet.isEmpty) {
       return const Scaffold(
         body: Center(
           child: Text('Please select a word set with terms and definitions')
@@ -52,7 +40,7 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
         child: Card(
           child: Center(
             child: Text(
-              _showDefinition ? _definitions[_index] : _terms[_index],
+              _showDefinition ? wordSet[_index].value : wordSet[_index].key,
               style: TextStyle(
                 fontSize: MediaQuery.sizeOf(context).longestSide / 20
               ),
@@ -74,13 +62,13 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
           ),
           const SizedBox(width: 4.0),
           FloatingActionButton(
-            onPressed: shuffle,
+            onPressed: () => setState(() => wordSet.shuffle()),
             child: const Icon(Icons.shuffle),
           ),
           const SizedBox(width: 4.0),
           FloatingActionButton(
             onPressed: () => setState(() {
-              if (_index != _terms.length - 1) {
+              if (_index != wordSet.length - 1) {
                 _index++;
                 _showDefinition = false;
               }
